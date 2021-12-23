fd1940e6f55dc61507b96998661301d4fdbb45b5
doitduri <kdle_@naver.com>
2021-12-23 20:57:53 +0900

    fix: Model 수정 및 user 생성
    - signIn
    - userInfo, user

--- MVVM+Authentication/Model/UserInfo.swift
+++ MVVM+Authentication/Model/UserInfo.swift
@@ -7,19 +7,30 @@
 
 import Foundation
 
-struct UserInfoResponse: Decodable {
+struct User: Codable {
     let id: Int
     let email: String
     let isAdmin: Bool
-    let password: String
     
     
     enum CodingKeys: String, CodingKey {
-        case id
-        case email
-        case isAdmin
-        case password
+        case id = "id"
+        case email = "email"
+        case isAdmin = "is_admin"
     }
     
+    init() {
+        self.id = -1
+        self.email = ""
+        self.isAdmin = false
+    }
     
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.id = try values.decodeIfPresent(Int.self, forKey: .id) ?? -1
+        self.email = try values.decodeIfPresent(String.self, forKey: .email) ?? ""
+        self.isAdmin = try values.decodeIfPresent(Bool.self, forKey: .isAdmin) ?? false
+    }
 }
+
